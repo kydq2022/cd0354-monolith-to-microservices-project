68911b9391bdbe7a178ed2336eddb89f2464c090
Ky Dinh <kydq2022@outlook.com>
2023-06-06 09:31:29 +0700

    update fe config

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ac9096c6570c84a829d87a101496e053-1971414216.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
